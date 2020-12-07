333f2623a3f2f06db2938726c810292e9dea3fa2
alexey <khlebtsov@gmail.com>
2020-12-07 18:23:00 +0100

    implements rest service to get pokemon

--- src/main/kotlin/com/khlebtsov/pokemon/features/shakespeare/service/rest/pokemon/Dtos.kt
+++ src/main/kotlin/com/khlebtsov/pokemon/features/shakespeare/service/rest/pokemon/Dtos.kt
@@ -1,7 +1,9 @@
-package com.khlebtsov.pokemon.features.shakespeare.service.rest.characteristics
+package com.khlebtsov.pokemon.features.shakespeare.service.rest.pokemon
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+
+//getCharacteristics
 data class HighestStatDto(val name: String, val url: String)
 data class LanguageDto(val name: String, val url: String)
 data class DescriptionDto(val description: String, val language: LanguageDto)
@@ -14,3 +16,8 @@ data class PokemonCharacteristicsServiceResponse(
     @JsonProperty("highest_stat") val highestStat: HighestStatDto,
     val descriptions: List<DescriptionDto>
 )
+
+//getPokemon
+data class GetPokemonRequest(val name: String)
+data class GetPokemonResponse(val id: Int, val name: String, val height: Int, val weight: Int)
+
---
+++ src/main/kotlin/com/khlebtsov/pokemon/features/shakespeare/service/rest/pokemon/PokemonRestService.kt
@@ -0,0 +1,31 @@
+package com.khlebtsov.pokemon.features.shakespeare.service.rest.pokemon
+
+import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.RestTemplate
+
+@Service
+class PokemonRestService(private val restTemplate: RestTemplate) {
+
+    val base: String = "https://pokeapi.co/api/v2"
+
+    fun getCharacteristics(request: PokemonCharacteristicsServiceRequest): PokemonCharacteristicsServiceResponse? {
+        return try {
+            restTemplate.getForEntity(
+                "$base/${request.id}/", PokemonCharacteristicsServiceResponse::class.java
+            ).body ?: throw IllegalStateException("Unexpected result, body is null")
+        } catch (notFoundException: HttpClientErrorException.NotFound) {
+            null
+        }
+    }
+
+    fun getPokemon(request: GetPokemonRequest): GetPokemonResponse? {
+        return try {
+            restTemplate.getForEntity(
+                "$base/pokemon/${request.name}/", GetPokemonResponse::class.java
+            ).body ?: throw IllegalStateException("Unexpected result, body is null")
+        } catch (notFoundException: HttpClientErrorException.NotFound) {
+            null
+        }
+    }
+}
